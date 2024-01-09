a82a3d4dd9a100f24a6a348629d6c5f6eed49433
Asharp19 <rishabh.3172000@gmail.com>
2024-01-09 23:28:53 +0530

    burnfee

--- src/TokenUpdateBEP20/GANJESToken.sol
+++ src/TokenUpdateBEP20/GANJESToken.sol
@@ -8,10 +8,10 @@ contract GanjesToken is ERC20, Ownable {
     uint256 public transactionFeePercent;// total transaction fee
     uint256 public liquidityFeePercent; // percentage of transaction fee allocated to liquidity
     address public liquidityWallet;
-    address public mockSupply;
+    address public initialSupplyAddress;
 
-    constructor(uint256 initialSupply, address _liquidityWallet, address _mockSupply, uint256 feePercent, uint256 liquidityPercent) ERC20("Ganjes", "GANJES") Ownable(msg.sender){
-        mockSupply=_mockSupply;
+    constructor(uint256 initialSupply, address _liquidityWallet, address _initialSupply, uint256 feePercent, uint256 liquidityPercent) ERC20("Ganjes", "GANJES") Ownable(msg.sender){
+        initialSupplyAddress=_initialSupply;
         _mint(mockSupply, initialSupply * (10 ** uint256(decimals())));
         liquidityWallet = _liquidityWallet;
         liquidityFeePercent=liquidityPercent;
@@ -32,17 +32,25 @@ contract GanjesToken is ERC20, Ownable {
         if (liquidityFee > 0) {
             super._transfer(_msgSender(), liquidityWallet, liquidityFee);
         }
+        if(burnerFee>0){
+            super._burn(_msgSender(),burnerFee);
+        }
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
         uint256 fee = (amount * transactionFeePercent) / 100;
         uint256 liquidityFee = (fee * liquidityFeePercent) / 100;
+        uint256 burnerFee=(fee * (100-liquidityFeePercent))/100;
         uint256 transferAmount = amount - fee;
 
         super.transferFrom(sender, recipient, transferAmount);
+
         if (liquidityFee > 0) {
             super._transfer(sender, liquidityWallet, liquidityFee);
+        }
+        if(burnerFee>0){
+            super._burn(sender,burnerFee);
         }
         return true;
     }
